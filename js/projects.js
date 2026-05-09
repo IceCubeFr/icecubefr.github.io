7e68a7d0264a09eb53719df78efb0e48c166c3f9
Florian GAVOILLE <icecubeof59@gmail.com>
2026-05-09 18:51:09 +0200

    mise à jour pour le rendu portfolio but2

--- js/projects.js
+++ js/projects.js
@@ -2,7 +2,7 @@ const PROJECTS_DATA = [
    {
     "title": "REST API",
     "imgUrl": "https://www.oracle.com/a/ocom/img/hp11-intl-java-logo.jpg",
-    "shortDesc": "Projet de développement d'un système d'API REST en Java (JEE) pour une déchetterie",
+    "shortDesc": "Projet de développement d'un système d'API REST en Java (JEE) pour un centre de recyclage",
     "tags": [
       { "title": "JEE", "type": "dev" },
       { "title": "Java", "type": "dev" },
@@ -11,7 +11,7 @@ const PROJECTS_DATA = [
       { "title": "BUT2", "type": "autre" }
     ],
     "desc": "Système API REST",
-    "descHTML": "La plateforme API REST a été développée en java en utilisant un serveur Tomcat pour gérer un système de déchetterie (type de déchets, point de collecte etc.) via des requêtes web (GET, POST, PUT, PATCH et DELETE). Ce sytème possède une base de données ainsi qu'un système de connexion par Token BASIC. Le projet, effectué en groupe de 2, est une application directe des cours de Web auxquels nous avons assisté pendant le semestre 4.",
+    "descHTML": "La plateforme API REST a été développée en java en utilisant un serveur Tomcat pour gérer un système de déchetterie (type de déchets, point de collecte etc.) via des requêtes web (GET, POST, PUT, PATCH et DELETE). Ce sytème possède une base de données ainsi qu'un système de connexion par Token BASIC. Le projet, effectué en groupe de 2, est une application directe des cours de Web auxquels nous avons assisté pendant le semestre 4. J'ai principalement participé au projet dans la création des endpoints (points d'entrée de l'API), dans la mise en place des requêtes SQL et dans le lien entre les deux par la base d'une architecture modèle-vue-contrôleur. J'ai pu y découvrir le fonctionnement d'une API, une introduction importante au développement WEB avancé.",
     "link": "https://github.com/IceCubeFr/sae-rest"
   },
    {
@@ -27,7 +27,7 @@ const PROJECTS_DATA = [
       { "title": "BUT2", "type": "autre" }
     ],
     "desc": "Jeu en typescript complexe",
-    "descHTML": "Le projet, effectué en groupe de 3, a été important dans l'application de notre apprentissage en JavaScript et TypeScript tout au long du court semestre 4. Nous devions utiliser les MileStones, Issue Board et Issues de GitLab pour gérer notre projet le mieux possible. La note finale a été basée sur la qualité du projet ainsi que sur le respect de la méthode de gestion de projet imposée",
+    "descHTML": "Le projet, effectué en groupe de 3, a été important dans l'application de notre apprentissage en JavaScript et TypeScript tout au long du semestre 4. Nous devions utiliser les MileStones, Issue Board et Issues de GitLab pour gérer notre projet le mieux possible en le séparant en petites tâches et en organisant temporellement chaque tâche à effectuer. La note finale a été basée sur la qualité du projet ainsi que sur le respect de la méthode de gestion de projet imposée. Ma participation a été principalement concentrée autour du style du jeu, de sa direction artistique et du lien entre le client et le serveur. Le travail a été, dans l'ensemble, très bien réparti entre tous les membres du groupe, un exemple de fonctionnement d'un projet de grande envergure.",
     "link": "https://github.com/IceCubeFr/Merge-Conflict"
   },
   {
@@ -42,7 +42,7 @@ const PROJECTS_DATA = [
       { "title": "BUT2", "type": "autre" }
     ],
     "desc": "Jeu de labyrinthe complexe.",
-    "descHTML": "Le projet consiste en la création d'un jeu de labyrinthe avec génération procédurale et résolution automatique par différents algorithmes. L'accent a été mis sur la qualité du code, le respect des patrons de conception (Design Patterns) et l'efficacité des algorithmes de recherche de chemin.",
+    "descHTML": "Le projet consiste en la création d'un jeu de labyrinthe avec génération procédurale et résolution automatique par différents algorithmes. L'accent a été mis sur la qualité du code, le respect des patrons de conception (Design Patterns) et l'efficacité des algorithmes de recherche de chemin. J'ai eu une place importante dans la gestion globale du projet, dans la lisaison du travail de chacun et dans la mise en page finale du projet.",
     "link": "https://github.com/IceCubeFr/S3.02"
   },
   {
@@ -52,17 +52,17 @@ const PROJECTS_DATA = [
     "tags": [
       { "title": "Java", "type": "dev" },
       { "title": "Agile", "type": "gestion" },
-      { "title": "Travail d'équipe", "type": "autre" },
+      { "title": "Travail d'équipe", "type": "gestion" },
       { "title": "BUT2", "type": "autre" }
     ],
     "desc": "Projet Agile SCRUM.",
-    "descHTML": "Développement d'un jeu en mode texte (ASCII City) en utilisant la méthodologie Agile SCRUM. Ce projet a permis de mettre en pratique la gestion de sprint, les daily meetings, et l'utilisation d'outils de gestion de projet en équipe.",
+    "descHTML": "Développement d'un jeu en mode texte (ASCII City) en utilisant la méthodologie Agile SCRUM. Ce projet a permis de mettre en pratique la gestion de sprint, les daily meetings, et l'utilisation d'outils de gestion de projet en équipe. Cette introduction à l'agilité a été un réel défi pour nous puisque nous n'avions jamais travaillé en suivant ce type de méthode auparavant, encore moins en travaillant sur un unique projet tous les jours de manière intense pendant quelques jours. Ce projet, bien que court, nous a apporté des connaissances sur les réalité du terrain, tant par le rythme que par la méthode de travail. J'ai eu une place importante dans la mise en place des user stories, dans le développement du projet dans son ensemble ainsi que dans sa gestion globale",
     "link": "https://github.com/IceCubeFr/projet-agile-s3"
   },
   {
     "title": "ElectionsSim",
     "imgUrl": "./media/ElectionSim.png",
-    "shortDesc": "Appli JavaFX pour gérer les élections numériques du SNU (Projet volontaire).",
+    "shortDesc": "Application JavaFX pour gérer les élections numériques du SNU (Projet volontaire).",
     "tags": [
       { "title": "Java", "type": "dev" },
       { "title": "JavaFX", "type": "dev" },
@@ -70,13 +70,13 @@ const PROJECTS_DATA = [
       { "title": "Personnel 2025", "type": "autre" }
     ],
     "desc": "Simulateur d'élections complet.",
-    "descHTML": "Ce simulateur d'élections a fait appel à mes compétences en Java, JavaFX et versioning que j'ai pu acquérir tout au long de ma première année de BUT. Via une interface graphique, nous avons la possibilité de lancer des élections, d'ajouter ou de retirer des voix, et de lancer des scénarios aléatoires remplaçant une partie des voix par des abstentions. L'objectif de ce projet était de montrer aux engagés du SNU l'impact de l'abstention sur les élections.",
+    "descHTML": "Ce simulateur d'élections a fait appel à mes compétences en Java, JavaFX et versioning que j'ai pu acquérir tout au long de ma première année de BUT. Via une interface graphique, nous avons la possibilité de lancer des élections, d'ajouter ou de retirer des voix, et de lancer des scénarios aléatoires remplaçant une partie des voix par des abstentions. L'objectif de ce projet était de montrer aux engagés du SNU l'impact de l'abstention sur les élections. J'ai été l'unique développeur sur ce projet et j'ai pu mettre en avant l'ensemble de mes connaissances à cette période du BUT.",
     "link": "https://github.com/IceCubeFr/ElectionsSim"
   },
   {
     "title": "Services Réseaux",
     "imgUrl": "./media/datacenter.png",
-    "shortDesc": "Mise en place d'un serveur et d'un système Gitea (similaire Github).",
+    "shortDesc": "Mise en place d'un serveur et d'un système Gitea (stockage de projets Git).",
     "tags": [
       { "title": "Serveur", "type": "dev" },
       { "title": "Gitea", "type": "dev" },
@@ -85,7 +85,7 @@ const PROJECTS_DATA = [
       { "title": "BUT1", "type": "autre" }
     ],
     "desc": "Installation de services réseaux.",
-    "descHTML": "Projet de mise en œuvre de services réseaux sous Linux. Installation et configuration d'un serveur Gitea pour la gestion de dépôts Git, ainsi que la rédaction de documentations techniques en Markdown. Une partie du projet a été réalisée en anglais.",
+    "descHTML": "Projet de mise en œuvre de services réseaux sous Linux. Installation et configuration d'un serveur Gitea pour la gestion de dépôts Git, ainsi que la rédaction de documentations techniques en Markdown. Une partie du projet a été réalisée en anglais. L'objectif de ce projet était principalement de nous apprendre à suivre des documentations, à en rédiger, à utiliser les languages de formattage léger (Markdown ou AsciiDoc) et de nous donner des compétences DevOps pour répondre à toutes les demandes client. En groupe de deux, ce projet a été globalement bien réparti entre tous les membres permettant un rendu final répondant totalement aux attentes. J'ai eu une place importante dans la rédaction des documentations.",
     "link": "https://github.com/IceCubeFr/S2.03"
   },
   {
@@ -96,11 +96,11 @@ const PROJECTS_DATA = [
       { "title": "SQL", "type": "dev" },
       { "title": "Statistiques", "type": "comm" },
       { "title": "Rapport", "type": "comm" },
-      { "title": "Travail équipe", "type": "autre" },
+      { "title": "Travail équipe", "type": "gestion" },
       { "title": "BUT1", "type": "autre" }
     ],
     "desc": "Analyse de données SQL.",
-    "descHTML": "Analyse d'une base de données relationnelle contenant les résultats historiques des Jeux Olympiques. Utilisation de requêtes SQL complexes pour extraire des statistiques pertinentes et rédaction d'un rapport de synthèse détaillé sur les performances des athlètes et des nations.",
+    "descHTML": "Analyse d'une base de données relationnelle contenant les résultats historiques des Jeux Olympiques. Utilisation de requêtes SQL complexes pour extraire des statistiques pertinentes et rédaction d'un rapport de synthèse détaillé sur les performances des athlètes et des nations. Alliant gestion de base de données et statistiques, ce projet nous a permis d'appliquer nos compétences récemmment acquises en SQL et statistiques sur un cas concret : l'étude statistique de l'ensemble des participants des Jeux Olympiques entre deux dates données. Dans ce projet, j'ai principalement travaillé sur la rédaction du rapport et la réalisation de certaintes requêtes SQL.",
     "link": "https://github.com/IceCubeFr/BDD-BUT1"
   },
   {
@@ -111,24 +111,24 @@ const PROJECTS_DATA = [
       { "title": "Java", "type": "dev" },
       { "title": "Java FX", "type": "dev" },
       { "title": "Graphes", "type": "dev" },
-      { "title": "Travail équipe", "type": "autre" },
+      { "title": "Travail équipe", "type": "gestion" },
       { "title": "BUT1", "type": "autre" }
     ],
     "desc": "Gestion d'échanges via graphes.",
-    "descHTML": "Développement d'une application JavaFX permettant de gérer des échanges de jeunes entre différents pays. Utilisation de la théorie des graphes pour optimiser les trajets et les affectations. L'interface utilisateur a été conçue pour être intuitive et fonctionnelle.",
+    "descHTML": "Développement d'une application JavaFX permettant de gérer des échanges de jeunes entre différents pays. Utilisation de la théorie des graphes pour optimiser les affectations en respectant les contraintes de chaque jeune. L'interface utilisateur a été conçue pour être intuitive et fonctionnelle. Ce projet important, réparti sur 3 mois, a été un point final sur la première année de BUT Informatique. Mêlant trois matières informatiques, à savoir Graphes, Développement orienté objet et IHM (Interface Homme Machine), rigueur, communication et organisation étaient essentiels au bon déroulé du projet. Plusieurs rapports étaient demandés en Graphes dans une première partie théorique, puis un logiciel fonctionnel sans interface en deuxième partie et enfin une interface fonctionnelle et optimale pour finir. J'ai activement participé dans l'ensemle du projet, mais mon travail a été marqué dans la partie théorique des Graphes et l'application IHM du logiciel. Le projet a été correctement réparti entre les 5 membres de l'équipe.",
     "link": "https://github.com/IceCubeFr/s201-s202"
   },
   {
     "title": "Intro BDD",
     "imgUrl": "./media/garage.jpeg",
-    "shortDesc": "Création d'un SI pour un garage auto avec Access et étude de marché.",
+    "shortDesc": "Création d'un SI pour un garage auto avec Microsoft Access et étude de marché.",
     "tags": [
       { "title": "Access", "type": "dev" },
       { "title": "Étude de marché", "type": "gestion" },
       { "title": "BUT1", "type": "autre" }
     ],
     "desc": "Système d'information de garage.",
-    "descHTML": "Conception d'un système d'information complet pour un garage automobile en utilisant Microsoft Access. Le projet incluait également une étude de marché préliminaire pour justifier les choix technologiques et fonctionnels du système.",
+    "descHTML": "Conception d'un système d'information complet pour un garage automobile en utilisant Microsoft Access. Le projet incluait également une étude de marché préliminaire pour justifier les choix technologiques et fonctionnels du système. Liant les premières connaissances acquises en base de données (MCD, MLD et tables access) avec l'économie, nous devions imaginer une entreprise fictive dans un domaine donné aléatoirement (et différent pour chaque groupe), en faire une étude de marché et lui créer un système d'informations. Le projet était rendu sur deux rapports, un pour l'économie et un pour la partie bases de données, cette dernière étant complétée par le rendu Access. Je me suis principalement chargée de la partie base de données dans ce projet, et mon coéquipier de la partie économie.",
     "link": "https://github.com/IceCubeFr/BDD-BUT1"
   },
   {
@@ -138,12 +138,12 @@ const PROJECTS_DATA = [
     "tags": [
       { "title": "iJava", "type": "dev" },
       { "title": "Gestion de projet", "type": "gestion" },
+      { "title": "Travail d'équipe", "type": "gestion" },
       { "title": "Anglais", "type": "ang" },
-      { "title": "Travail d'équipe", "type": "autre" },
       { "title": "BUT1", "type": "autre" }
     ],
     "desc": "Jeu pédagogique bilingue.",
-    "descHTML": "Création d'un jeu sérieux (serious game) bilingue français-anglais développé en iJava. Le jeu vise à faciliter l'apprentissage de la langue anglaise de manière ludique. Ce projet a nécessité une forte coordination au sein de l'équipe de développement.",
+    "descHTML": "Création d'un jeu ludo-pédagogique bilingue français-anglais développé en iJava (version itérale de Java, créé et maintenu par l'IUT de Lille). Le jeu vise à faciliter l'apprentissage de la langue anglaise de manière ludique. Ce projet a nécessité une forte coordination au sein de l'équipe de développement. Ce premier projet en développement était un premier pas sur la gestion de projet. Un git existait mais n'était pas exploité à son potentiel, le projet ne tenait que sur un seul fichier, iJava n'étant pas pensé orienté objet, et l'ensemble n'était pas optimisé. Malgré tous ces défauts, ce projet a été un premier pas dans le monde de la gestion de projet et dans la mise en place d'une application complète répondant à une demande. En groupe de deux, je me suis principalement chargé du développement pendant que j'étais épaulé pour la partie fonctionnel (histoire, fonctionnalités et conception). Aucun rapport n'était attendu, uniquement le projet fini.",
     "link": "https://github.com/IceCubeFr/Double-Langue"
   },
   {
@@ -157,7 +157,7 @@ const PROJECTS_DATA = [
       { "title": "BUT1", "type": "autre" }
     ],
     "desc": "Plateforme de mobilité durable.",
-    "descHTML": "Réalisation d'un site web statique pour une plateforme interne de location de véhicules électriques et de covoiturage au sein d'Ubisoft. Le projet a couvert toutes les phases de conception, du maquettage à la communication visuelle.",
+    "descHTML": "Réalisation d'un site web statique pour une plateforme interne de location de véhicules électriques et de covoiturage au sein d'Ubisoft. Le projet a couvert toutes les phases de conception, du maquettage à la communication visuelle. Ce premier projet de BUT Informatique nous a introduit les bases du web, html et css. Nous devions concevoir un site pour une entreprise du monde de l'informatique en imaginant ce que pourrait faire cette entreprise si elle souhaitait faire une plateforme de covoiturage ou de location de véhicule verts légers. Nous avons donc choisi, avec mon équipe, un site autour d'Ubisoft de location de véhicules verts légers. Je me suis principalement occupé du développement du squelette et d'une partie du style du site, pendant que mes coéquipiers m'ont épaulé et se sont chargés de l'étude de l'entreprise pour la partie gestion.",
     "link": "./Ubisoft_Location/index.html"
   },
   {
@@ -171,7 +171,7 @@ const PROJECTS_DATA = [
       { "title": "Projet Personnel 2024", "type": "autre" }
     ],
     "desc": "Plugin de partage C#.",
-    "descHTML": "Développement autodidacte d'un plugin en C# pour le jeu NovaLife:Amboise. Ce plugin permet le partage de fichiers ou de données entre les joueurs à proximité, améliorant ainsi l'expérience multijoueur du serveur.",
+    "descHTML": "Mes premiers pas en développement informatique. Après avoir appris par moi-même les bases du C#, je me suis lancé dans le développement d'un plugin pour le jeu NovaLife:Amboise. Ce plugin permet le partage de numéro de téléphone entre les joueurs à proximité, améliorant ainsi l'expérience multijoueur du serveur. Ce petit jeu de roleplay, anciennement développé par un studio français et désormais laissé à l'abandon, laissait la possibilité aux joueurs de développer des plugins, des petits programmes ajoutant des fonctionnalités au jeu. Propres aux serveurs, ces plugins pouvaient apporter une réelle valeur ajoutée et étaient très importants autour du jeu. Fort de mes cours de NSI au lycée, je me suis lancé dans l'apprentissage des bases du C#, laissant néanmoins la partie orientée objet de côté. Ce projet a été développé entièrement par mes soins.",
     "link": "https://github.com/IceCubeFr/NearbyShare"
   }
 ];
